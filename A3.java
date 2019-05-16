62616bbd350133366b8862673fa953b089c73dd1
Carlos Portillo <42984331+cpxrtillo@users.noreply.github.com>
2019-05-15 19:38:05 -0700

    Add files via upload

--- A3.java
+++ A3.java
@@ -1,179 +1,158 @@
 import java.util.*;
 import java.io.*;
 
-public class A3 {
-		
-    public static void main(String[] args) {
-    	
-        A3 start = new A3();
-        String inputFile = "tmdb_5000_credits.csv";
-        start.readFile(inputFile);
-    }
-        
-    public void readFile(String inputFile) {
-    	
-    	HashMap<String, HashSet<String>> graph = new HashMap<String, HashSet<String>>();
-    	
-    	try {
-    		
-    		BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-    		Scanner user = new Scanner(System.in);
-            String line;
-            line = reader.readLine();
-            String cast;
-            String name = "";
-            String startindex = "";
-            HashSet<String> valuenameset = null;
-            String keyname = "";
-            
-            // read in each "movie line" at a time
-            while ((line = reader.readLine()) != null) {
-            	
-            	// check to see if whatever's inside the square brackets inside file is null
-                if (line.charAt(line.indexOf("[") + 1) == ']') {
-                    continue;
-                }
-                
-                // if not, get inside the brackets and remove double quotes
-                else {
-                    cast = line.substring(line.indexOf(",\"[") + 2, line.indexOf("]\",") + 1);
-                    cast = cast.replaceAll("\"\"", "\"");                
-                }
-                
-                // split lines up in string array, convert to arraylist, initialize hashset
-                String[] linenames = cast.split("},");
-                List<String> movielinenames = new ArrayList<String>();
-                movielinenames = Arrays.asList(linenames);
-                
-                // for however many names there are in the movie line
-                for (int j = 0; j < movielinenames.size(); j++) {
-                	
-                	// get specific names
-                    startindex = movielinenames.get(j);
-                    int namesindex = (startindex.indexOf("name")) + 8;
-                    int endindex = (startindex.indexOf(",", namesindex)) - 1;
-                    
-                    // if there are true indices for start and end of a name
-                    if (namesindex > -1 && endindex > -1) {
-                    	
-                    	// create name
-                    	name = startindex.substring(namesindex, endindex);
-                    	keyname = startindex.substring(namesindex, endindex);
-                    	
-                    	if (graph.containsKey(keyname) == false) {
-                    		
-                    		// create new set for new name
-                    		valuenameset = new HashSet<String>();
-                    		
-                    		// get all names in that movie and add them to person's set
-                    		for (int x = 0; x < movielinenames.size(); x++) {
-                    		
-                    			// grab xth term in the line which is already split, computes the beginning and last indices of the name
-                    			startindex = movielinenames.get(x);
-                                namesindex = (startindex.indexOf("name")) + 8;
-                                endindex = (startindex.indexOf(",", namesindex)) - 1;
-                                
-                                // if there are true indices for start and end of a name
-                                if (namesindex > -1 && endindex > -1) {
-                                	if(name == keyname) {
-                                		graph.put(keyname, null);
-                                		break;
-                                	}
-                                	else {
-	                                	name = startindex.substring(namesindex, endindex);
-		                    			valuenameset.add(name);
-		                    			graph.put(keyname, valuenameset);
-                                	}
-                                }
-                    		}
-                    	}
-                    	
-                    	else {
-                    		// retrieve existing set of names of the specific actor in the line we are reading in
-                    		HashSet<String> existingvalues = graph.get(keyname);
-                    		
-                    		// if name existed already, get every single person in that new movie, add to their own set they've worked with
-                    		// starting from the beginning of the line where which we read in
-                    		for (int l = 0; l < movielinenames.size(); l++) {
-                    			
-                    			startindex = movielinenames.get(l);
-                                namesindex = (startindex.indexOf("name")) + 8;
-                                endindex = (startindex.indexOf(",", namesindex)) - 1;
-                               
-                                if (namesindex > -1 && endindex > -1) {
-                                	if(name == keyname) {
-                                		graph.put(keyname, null);
-                                		break;
-                                	}
-                                	else {
-		                            	name = startindex.substring(namesindex, endindex);
-		                    			existingvalues.add(name);
-		    	                        graph.put(keyname, existingvalues);
-                                	}
-                                }
-                    		}
-                    	}
-                    }
-                }
-            }
-          
-            // prompt for names of actors to find path
-            System.out.println("Actor 1: ");
-            String actor1 = user.nextLine();
-            System.out.println("Actor 2: ");
-            String actor2 = user.nextLine();
-            
-            if(graph.containsKey(actor1) == false || graph.containsKey(actor2) == false) {
-            	System.out.println("One or both of the actors you want to find do not exist in this file.");
-            }
-            else {
-            	bfs(graph, actor1, actor2);
-            }
-    	}
-    	
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void bfs(HashMap<String, HashSet<String>> graph, String actor1, String actor2) {
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-    	ArrayList<String> visited = new ArrayList<String>();
-    	Queue<String> queue = new LinkedList<String>();
-    	Hashtable<String, String> path = new Hashtable<String, String>();
-    	
-    	path.put(actor1, "");
-    	queue.add(actor1);
-    	visited.add(actor1);
-    	String current = "";
-    	
-    	while(!queue.isEmpty()) {
-    		
-    		String next = queue.remove();
-    		
-    		Set<String> list = neighbors(next, graph);
-    		visited.add(next);
-			Iterator<String> listit = list.iterator();
-		
-			if (list.contains(actor2)) {
-				path.put(actor1, next);
-				break;
-			}
+public class A3 {
+
+	public static void main(String[] args) {
+		A3 start = new A3();
+		String inputFile = "tmdb_5000_credits.csv";
+		start.readFile(inputFile);
+	}
+
+	public void readFile(String inputFile) {
+
+		try {
+
+			Scanner scans = new Scanner(System.in);
+			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+			CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
+					.withHeader("movie_id", "title", "cast", "crew").withFirstRecordAsHeader().withTrim());
+			JSONParser jsonParser = new JSONParser();
+			HashMap<String, HashSet<String>> graph = new HashMap<String, HashSet<String>>();
 			
-			else {
-				while(listit.hasNext()) {
-					if(!visited.contains(actor2)) {
-						current = listit.next();					
-						queue.add(current);
-						visited.add(current);
+			// for every movie line, create JSONArray of "cast" column
+			for (CSVRecord record : csvParser) {
+
+				// make JSONArray of "cast" column
+				JSONArray jsonarr = (JSONArray) jsonParser.parse(record.get("cast"));
+
+				// go through the JSONArray which has the names from cast column, make them
+				// JSONObjects
+				// initialize name sets for keys
+				HashSet<String> newnames = null;
+				HashSet<String> existingnames = null;
+
+				for (int i = 0; i < jsonarr.size(); i++) {
+					JSONObject name = new JSONObject();
+					name = (JSONObject) jsonarr.get(i);
+
+					String vactor = (String) name.get("name");
+					vactor = vactor.toLowerCase().trim();
+
+					// if there is no key for the actor, create key and new set
+					if (graph.containsKey(vactor) == false) {
+						newnames = new HashSet<String>();
+						graph.put(vactor, newnames);
+
+						for (int x = 0; x < jsonarr.size(); x++) {
+							name = new JSONObject();
+							name = (JSONObject) jsonarr.get(x);
+							String coactor = (String) name.get("name");
+							coactor = coactor.toLowerCase().trim();
+							newnames.add(coactor);
+						}
+						graph.put(vactor, newnames);
+					}
+
+//    	        	else, get their existing names in their key sets, add names to existing values
+					else if (graph.containsKey(vactor)) {
+						existingnames = graph.get(vactor);
+						for (int x = 0; x < jsonarr.size(); x++) {
+							name = new JSONObject();
+							name = (JSONObject) jsonarr.get(x);
+							String coactor = (String) name.get("name");
+							coactor = coactor.toLowerCase().trim();
+							existingnames.add(coactor);
+						}
+						graph.put(vactor, existingnames);
 					}
 				}
 			}
-    	}
-    	System.out.println("Path between " + actor1 + " and " + actor2 + ": " + actor1 + path.get(actor1));
-    }
-    
-    public Set<String> neighbors(String neighbs, HashMap<String, HashSet<String>> graph) {
-    	return graph.get(neighbs);
+			
+			// prompt for names of actors to find path
+			System.out.println("Choose actor 1: ");
+			String actor1 = scans.nextLine();
+			actor1 = actor1.toLowerCase();
+			
+			System.out.println("Choose actor 2: ");
+			String actor2 = scans.nextLine();
+			actor2 = actor2.toLowerCase();
+
+			reader.close();
+			scans.close();
+			csvParser.close();
+
+			if (graph.containsKey(actor1) == false) {
+				System.out.println(actor1 + " does not exist in this file.");
+			}	
+			else if(graph.containsKey(actor2) == false) {
+				System.out.println(actor2 + " does not exist in this file.");
+			} else {
+				System.out.println("1: " + graph.get(actor1));
+				System.out.println("2: " + graph.get(actor2));
+				bfs(graph, actor1, actor2);
+			}
+		}
+
+		catch (IOException | ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void bfs(HashMap<String, HashSet<String>> graph, String actor1, String actor2) {
+
+		Hashtable<String, Boolean> visited = new Hashtable<String, Boolean>();
+		Hashtable<String, String> path = new Hashtable<String, String>();
+		Queue<String> queue = new LinkedList<String>();
+		Hashtable<String, String> finalpath = new Hashtable<String, String>();
+
+		queue.add(actor1);
+
+		for (String name : graph.keySet()) {
+			visited.put(name, false);
+		}
+
+		// while queue is not empty
+		while (!queue.isEmpty()) {
+			// actor = dequeue from queue
+			String actor = queue.remove();
+			// iterate through actor's neighbors
+			for (String coactor : graph.get(actor)) {
+
+				// if not visit:
+				// put into queue, update path and visit table
+				if (!visited.get(coactor)) {
+					queue.add(coactor);
+					path.put(actor1, coactor);
+					visited.put(actor1, true);
+
+					// if equals des/to/actor2:
+					// print path table, generate path from path table
+					// used a loop in order to store the people in a new container called final path and then printed that path out
+					if (coactor.equals(actor2)) {
+						path.put(actor2, actor);
+						String s = path.get(actor1);
+						while (!s.equals(actor1)) {
+							System.out.println(s);
+							s = path.get(s);
+						}
+						return;
+					}
+				}
+			}
+		}
+		return;
+	}
+
+	public Set<String> neighbors(String neighbs, HashMap<String, HashSet<String>> graph) {
+		return graph.get(neighbs);
 	}
 }
